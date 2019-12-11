1414a51feb377dc756a63dbfb213995c76aa7595
EkaterinaYa8 <bkates@mail.ru>
2019-12-11 11:19:07 +0300

    fix test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
@@ -5,44 +6,41 @@ from .pages.basket_page import BasketPage
 from .pages.all_products_page import AllProductsPage
 
 
-def test_guest_can_go_to_login_page(browser, link):
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser, link):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
+    def test_guest_should_see_login_link(self, browser, link):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-def test_guest_should_see_login_link(browser, link):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_can_go_to_login_page(self, browser, link):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser, link):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_not_be_product_in_basket()
+        basket_page.should_be_empty_basket_message()
 
-def test_guest_can_go_to_login_page(browser, link):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, link):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_be_product_in_basket()
-    basket_page.should_be_empty_basket_message()
-
-
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser, link):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_all_products_page()
-    all_products_page = AllProductsPage(browser, browser.current_url)
-    all_products_page.go_to_product_page()
-    product_page = ProductPage(browser, browser.current_url)
-    product_page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_be_product_in_basket()
-    basket_page.should_be_empty_basket_message()
-
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser, link):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_all_products_page()
+        all_products_page = AllProductsPage(browser, browser.current_url)
+        all_products_page.go_to_product_page()
+        product_page = ProductPage(browser, browser.current_url)
+        product_page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_not_be_product_in_basket()
+        basket_page.should_be_empty_basket_message()
